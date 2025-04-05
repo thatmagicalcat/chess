fd12793f32bf989c13f66084bbb949b94f3a8855
thatmagicalcat <thatmagicalcat@hotmail.com>
2025-04-05 22:46:36 +0530

    added cell highlighting for movable squares

--- src/shaders/grid.glsl
+++ src/shaders/grid.glsl
@@ -18,20 +18,32 @@ out vec4 frag_color;
 
 const float STEP = 1.0 / 8.0;
 const vec4 COLORS[] = {
-        vec4(8.0, 8.0, 8.0, 1.0),
-        vec4(0.2, 0.2, 0.2, 1.0)
-    };
+    vec4(0.8, 0.8, 0.8, 1.0), // WHITE
+    vec4(0.2, 0.2, 0.2, 1.0), // BLACK
+};
+
+const vec4 ACTIVE_PIECE_COLOR = vec4(0.1, 0.6, 0.1, 1.0);
+const vec4 MOVABLE_PIECE_COLOR = vec4(0.0, 0.4, 0.4, 1.0);
 
 uniform ivec2 active_piece;
 
-void main() {
-    int x = int(floor(frag_coord.x / STEP));
-    int y = int(floor(frag_coord.y / STEP));
+// x: ms part, y: ls part
+uniform uvec2 piece_moves;
 
-    // base color
-    if (x == active_piece.x && y == active_piece.y) {
-        frag_color = vec4(0.1, 0.6, 0.1, 1.0);
+void main() {
+    int col = int(frag_coord.x / STEP);
+    int row = int(frag_coord.y / STEP);
+
+    int square = row * 8 + col;
+
+    if (
+        (square <= 32 && (piece_moves.y & (1 << square)) != 0)
+        || (square > 32 && (piece_moves.x & (1 << (square - 32))) != 0)
+    ) {
+        frag_color = MOVABLE_PIECE_COLOR;
+    } else if (col == active_piece.x && row == active_piece.y) {
+        frag_color = ACTIVE_PIECE_COLOR;
     } else {
-        frag_color = COLORS[(x + y) % 2];
+        frag_color = COLORS[(row + col) % 2];
     }
 }
